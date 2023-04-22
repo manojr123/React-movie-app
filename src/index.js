c55427c0f1c028d871228f4c898d2dec00946557
manojr123 <manojraghavan2008@yahoo.co.in>
2023-04-22 13:33:44 +0530

    Adding UI

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
+import App from './components/App';
+import movies from './reducers';
 
+const store = createStore(movies);
+console.log('store', store);
+console.log('BEFORE STATE', store.getState());
+
+
+store.dispatch({
+  type :'ADD_MOVIES',
+  movies : [{name: 'Superman '}]
+})
+
+console.log('AFTER STATE', store.getState());
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -11,7 +23,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
