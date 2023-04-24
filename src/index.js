a218be54139ca0e92f604a0732bf7140522d4230
manojr123 <manojraghavan2008@yahoo.co.in>
2023-04-24 16:47:49 +0530

    Using middleware

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import App from './components/App';
 import rootReducer from './reducers';
 
+// function logger(obj, next, action)
+// logger(obj)(next)(action) ...internally
+const logger = function ({dispatch, getState}) {
+  return function(next) {
+    return function(action) {
+      //middleware code
+      console.log('ACTION_TYPE = ', action.type);
+      next(action);
+    }
+  }
 
-const store = createStore(rootReducer);
+}
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 console.log('store', store);
 
 
